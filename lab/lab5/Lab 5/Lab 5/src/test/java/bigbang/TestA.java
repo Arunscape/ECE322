16c6cae3a3d2292e421101e346ebc003a05fa8f9
Arunscape <8227297+Arunscape@users.noreply.github.com>
2019-11-20 23:56:17 -0700

    I NEED TO SLEEP

--- lab/lab5/Lab 5/Lab 5/src/test/java/bigbang/TestA.java
+++ lab/lab5/Lab 5/Lab 5/src/test/java/bigbang/TestA.java
@@ -1,20 +1,26 @@
 package bigbang;
 
-import modules.ModuleB;
-import modules.ModuleC;
-import modules.ModuleD;
-import modules.ModuleE;
+import modules.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyString;
+
 
 public class TestA {
 
+    ModuleA ma;
     ModuleB mb;
     ModuleC mc;
     ModuleD md;
     ModuleE me;
+
+    ByteArrayOutputStream stdout;
 
     @BeforeEach
     public void setUp(){
@@ -22,8 +28,56 @@ public class TestA {
         mc = Mockito.mock(ModuleC.class);
         md = Mockito.mock(ModuleD.class);
         me = Mockito.mock(ModuleE.class);
+
+        ma = new ModuleA(mb, mc, md, me);
+
+        newStdout();
+
+    }
+
+    public void newStdout(){
+        stdout = new ByteArrayOutputStream();
+        ma.setOutputStream(new PrintStream(stdout));
     }
 
     @Test
+    public void testHelp() throws ModuleE.DataBaseExitException {
+        ma.run(new String[]{"help"});
+
+        final String help = "Available Commands: \n" +
+                "load <filepath>\n" +
+                "add <name> <number>\n" +
+                "update <index> <name> <number>\n" +
+                "delete <index>\n" +
+                "sort\n" +
+                "exit";
+
+        assertEquals(help + "\n", stdout.toString());
+    }
+
+    @Test
+    public void testLoad() throws ModuleE.DataBaseExitException {
+
+        final String TEST_FILENAME = "testFileName";
+
+        ma.run(new String[]{"load", TEST_FILENAME});
+    }
+
+    @Test
+    public void testLoadNoArgument() throws ModuleE.DataBaseExitException {
+
+            ma.run(new String[]{"load"});
+            assertEquals("Malformed command!\n", stdout.toString());
+
+
+    }
+
+    @Test
+    public void testLoadBReturnsNull() throws ModuleE.DataBaseExitException {
+        Mockito.when(mb.loadFile(anyString())).thenReturn(null);
+        final String TEST_FILENAME = "testFileName";
+
+        ma.run(new String[]{"load", TEST_FILENAME});
+    }
 
 }
